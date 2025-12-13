51ba6f242fb912e5f03b0050177bd7c5e8042674
zhangzhenhu <zhangzhenhu@enn.cn>
2025-12-13 22:32:21 +0800

    继续修复问题

--- clash_patch/hub/route/yml.go
+++ clash_patch/hub/route/yml.go
@@ -2,10 +2,12 @@ package route
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	// "path/filepath"
     C "github.com/Dreamacro/clash/constant"
+	"github.com/Dreamacro/clash/log"
 	"github.com/Dreamacro/clash/hub/executor"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
@@ -24,23 +26,36 @@ func ymlRouter() http.Handler {
 }
 
 func getYml(w http.ResponseWriter, r *http.Request) {
+	// 添加调试输出
+	// fmt.Println("Debug: getYml function called")
+	
 	data, err := ioutil.ReadFile(C.Path.Config())
 	if err != nil {
 		render.Status(r, http.StatusInternalServerError)
 		render.JSON(w, r, newError(err.Error()))
+		fmt.Printf("Error [getYml] 配置信息读取错误: %s\n", err)
+		log.Errorln("[getYml] 配置信息读取错误: %s", err)
 		return
 	}
+	fmt.Printf("Info [getYml] 读取配置信息完成，大小: %d 字节\n", len(data))
+	log.Errorln("[getYml] 读取配置信息完成，大小: %d 字节", len(data))
 	w.Header().Set("Content-Type", "application/x-yaml")
 	w.Write(data)
 }
 
 func getYmlAsJson(w http.ResponseWriter, r *http.Request) {
+	// 添加调试输出
+	// fmt.Println("Debug: getYmlAsJson function called")
+	
 	data, err := ioutil.ReadFile(C.Path.Config())
 	if err != nil {
 		render.Status(r, http.StatusInternalServerError)
 		render.JSON(w, r, newError(err.Error()))
+		log.Errorln("[getYmlAsJson] 配置信息读取错误: %s", err)
 		return
 	}
+	// fmt.Printf("Info [getYml] 读取配置信息完成，大小: %d 字节\n", len(data))
+	log.Infoln("[getYmlAsJson] 读取配置信息完成，大小: %d 字节", len(data))
 	var ymlData map[interface{}]interface{}
 	if err := yaml.Unmarshal(data, &ymlData); err != nil {
 		render.Status(r, http.StatusInternalServerError)
@@ -82,6 +97,9 @@ func convertToStringSlice(input []interface{}) []interface{} {
 	return output
 }
 func updateYml(w http.ResponseWriter, r *http.Request) {
+	// 添加调试输出
+	fmt.Println("Debug: updateYml function called")
+	
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		render.Status(r, http.StatusBadRequest)
@@ -98,31 +116,47 @@ func updateYml(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, r, newError(err.Error()))
 		return
 	}
+	fmt.Println("Info: 配置更新成功")
+	log.Infoln("Info: 配置更新成功")
 	render.JSON(w, r, map[string]string{"status": "success"})
 }
 
 func updateYmlFromJson(w http.ResponseWriter, r *http.Request) {
+	// 添加调试输出
+	// fmt.Println("Debug: updateYmlFromJson function called")
+	
 	var jsonData map[string]interface{}
+	// fmt.Println("Info [updateYmlFromJson] 接收到配置信息")
+	
 	if err := json.NewDecoder(r.Body).Decode(&jsonData); err != nil {
 		render.Status(r, http.StatusBadRequest)
 		render.JSON(w, r, newError(err.Error()))
+		fmt.Printf("Error [updateYmlFromJson] 错误信息: %s\n", err)
+		log.Infoln("[updateYmlFromJson] 错误信息: %s", err)
 		return
 	}
+	log.Infoln("[updateYmlFromJson] 接收到配置信息: %s", jsonData)
+
 	ymlData, err := yaml.Marshal(jsonData)
 	if err != nil {
 		render.Status(r, http.StatusInternalServerError)
 		render.JSON(w, r, newError(err.Error()))
+		log.Errorln("[updateYmlFromJson] yaml错误: %s", err)
 		return
 	}
 	if _, err := executor.ParseWithBytes(ymlData); err != nil {
 		render.Status(r, http.StatusBadRequest)
 		render.JSON(w, r, newError(err.Error()))
+		log.Errorln("[updateYmlFromJson] 校验错误: %s", err)
 		return
 	}
 	if err := ioutil.WriteFile(C.Path.Config(), ymlData, 0644); err != nil {
 		render.Status(r, http.StatusInternalServerError)
 		render.JSON(w, r, newError(err.Error()))
+		log.Errorln("[updateYmlFromJson] 文件写入错误: %s", err)
 		return
 	}
+	// fmt.Println("Info: JSON配置更新成功")
+	log.Infoln("Info: JSON配置更新成功")
 	render.JSON(w, r, map[string]string{"status": "success"})
 }
